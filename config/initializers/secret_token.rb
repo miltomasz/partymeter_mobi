f073eb2f7d5d1454ca1d7efeaf7c4bb77c230325
Tomasz Milczarek <miltomasz@mac>
2012-09-29 16:53:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartymeterMobi::Application.config.secret_token = '17c0fdc9fe781dfe71095abab32a3df19a552150952c4075528dd78e24812bf57b8a9d4a26e575110ebb1c5d6c4bf513c9807b44b4208212483a301bcf9fc5a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PartymeterMobi::Application.config.session_store :cookie_store, key: '_partymeter_mobi_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PartymeterMobi::Application.config.session_store :active_record_store
